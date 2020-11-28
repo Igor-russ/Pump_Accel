5f91469943c0a732f6fe335549c63992bd127d53
Igor-russ <mr.sim@mail.ru>
2020-11-28 23:52:37 +0400

    Working stage

---
+++ include/fast_functions.h
@@ -0,0 +1,16 @@
+#ifndef __FAST_FUNCTIONS_H
+#define __FAST_FUNCTIONS_H
+#include <Arduino.h>
+extern const int LCD_ADDR;
+// LCD fast functions
+void lcd_send(uint8_t, uint8_t);
+inline void lcd_command(uint8_t);
+inline void lcd_printChar(char);
+void lcd_setCursor(uint8_t, uint8_t);
+void lcd_print(char*);
+// Working with pins fast functions
+void pinMod(uint8_t, uint8_t);
+void pinWrite(uint8_t, bool);
+bool pinRead (uint8_t);
+void pinToggle(uint8_t);
+#endif
